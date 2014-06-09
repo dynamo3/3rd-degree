c10bd836f9aa2cb81d5005f7a772bc33df5d3cd4
glenh <glenhagy@gmail.com>
2014-06-09 16:53:59 -0700

    added some basic login functions

--- login.php
+++ login.php
@@ -5,44 +5,39 @@ include('db.php');
 // Start Database Object
 $db = new DB();
 
-$email = $_GET['email'];
-$password = $_GET['password'];
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+//  check for valid email address before searching database.
+// re = /^(.)+(\@)+(.)+(\.)+(.{1,64})$/;
+// if $email !
 
 // Write SQL Statement
-$sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+$sql = "SELECT * FROM user WHERE email=\"{$_POST['email']}\"";
 
 // Execute SQL Statement
 $results = $db->execute($sql);
 
-// Loop over results and make a $row variable for each result
-while ($row = $results->fetch_assoc()) {
-    echo    $row['name'] . ' | ' .
-            $row['street_address'] . ' | ' .
-            $row['zip_code'] . ' | ' .
-            $row['phone'] . ' | ' .
-            $row['category'] . ' | ' .
-            $row['twitter_name'] . '<br>';
+// print_r($results);
 
+// check for a matching entry for a registered user
+if ($results->num_rows != 0) {
+    // make a $row variable for results
+    $row = $results->fetch_assoc();
 
-<?php
-    $con=mysqli_connect("rockit.ccyd2ljjwzet.us-east-1.rds.amazonaws.com","3rddegree","rockit","3rddegree");
-    // Check connection
-    if (mysqli_connect_errno()) {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    // echo $row['id'] . ' | ' .
+    //      $row['email'] . ' | ' .
+    //      $row['password'] . '<br>';
+
+    if ($password == $row['password']) {
+        echo $row['email'] . ' successfully logged in. <br>';
+    } else {
+        echo $row['email'] . ' password did not match. <br>';
     }
+} else {
+    echo $_POST['email'] . ' is an unknown user, please register. <br>';
+}
 
-    // escape variables for security
-    $email = mysqli_real_escape_string($con, $_POST['email']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
 
-
-    $sql="SELECT FROM user (email, password)
-    VALUES ('$email', '$password')";
-
-    // if (!mysqli_query($con,$sql)) {
-    //   die('Error: ' . mysqli_error($con));
-    // }
-    // echo "1 record added";
-
-    // mysqli_close($con);
 ?>
