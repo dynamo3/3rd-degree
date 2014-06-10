d3896fe1ba6a016db652d17040afd0edd9a4722e
glenh <glenhagy@gmail.com>
2014-06-10 16:23:47 -0700

    added check for duplicate registration

--- register.php
+++ register.php
@@ -1,22 +1,42 @@
 <?php
-    $con=mysqli_connect("rockit.ccyd2ljjwzet.us-east-1.rds.amazonaws.com","3rddegree","rockit","3rddegree");
-    // Check connection
-    if (mysqli_connect_errno()) {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// Include Database Class
+include('db.php');
+
+// Start Database Object
+$db = new DB();
+// check for valid email address before searching database.
+
+$reg = '/^[a-zA-Z-_.+]+@[a-zA-Z-_.+]+\.[a-z]{2,6}\.?[a-z]+/';
+
+if (preg_match($reg, $_POST['email']) === 1) {
+
+    // echo ' valid email address <br>';
+
+    // Write SQL Statement
+    $sql = "SELECT * FROM user 
+    WHERE email=\"{$_POST['email']}\"";
+
+    // Execute SQL Statement
+    $results = $db->execute($sql);
+
+    // print_r($results);
+
+    // check for a matching entry for a registered user
+    if ($results->num_rows != 0) {
+
+        // already registered, might offer password recovery from here.
+        echo ' Sorry, this email address is already registered. <br>';
+    } else {
+
+        $sql="INSERT INTO user (email, password)
+        VALUES (\"{$_POST['email']}\", \"{$_POST['password']}\")";
+
+        $results = $db->execute($sql);
+
+        echo "1 record added";
+        
     }
 
-    // escape variables for security
-    $email = mysqli_real_escape_string($con, $_POST['email']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
+}
 
-
-    $sql="INSERT INTO user (email, password)
-    VALUES ('$email', '$password')";
-
-    if (!mysqli_query($con,$sql)) {
-      die('Error: ' . mysqli_error($con));
-    }
-    echo "1 record added";
-
-    mysqli_close($con);
-    ?>
+?>
