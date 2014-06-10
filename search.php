1c0665fee74f9f48e03e17c3724748f74a48177a
drpalmer23 <drpalmer23@gmail.com>
2014-06-10 14:19:39 -0700

    all category working in query

--- search.php
+++ search.php
@@ -6,10 +6,20 @@ include('db.php');
 // Start Database Object
 $db = new DB();
 
-
+if($_POST['category']=='All') {
+    $cat = "%";
+} else {
+    $cat = $_POST['category'];
+}
 
 // Write SQL Statement
-$sql = "SELECT * FROM restaurants as r, tweets as t, rating as ra WHERE r.zip_code=\"{$_POST['zip_code']}\" AND r.category=\"{$_POST['category']}\" AND r.twitter_name=t.twitter_name AND r.id=restaurant_id";
+$sql = "SELECT * FROM 
+    restaurants as r, tweets as t, rating as ra 
+    WHERE r.zip_code=\"{$_POST['zip_code']}\" 
+    AND r.category like \"$cat\" 
+    AND r.twitter_name=t.twitter_name 
+    AND r.id=ra.restaurant_id";
+
 // Execute SQL Statement
 $results = $db->execute($sql);
 
