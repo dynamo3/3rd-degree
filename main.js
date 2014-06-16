60227892deb5660e61cc1be4522e8c9a3110d4d9
drpalmer23 <drpalmer23@gmail.com>
2014-06-16 14:16:12 -0700

    search results now returning pins onto map

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-$(function() {
+;var App = App || {};
+(function($) {
+
    //-------drop down box for register and login --------
     $(".register").click(function(){
         $(".toggle").toggle("blind");
@@ -31,7 +33,7 @@ $(function() {
         var badColor = "#ff6666";
         //Compare the values in the password field 
         //and the confirmation field
-            if(pass1.value == pass2.value){
+            if(pass1.value == pass2.value) {
                 //The passwords match. 
                 //Set the color to the good color and inform
                 //the user that they have entered the correct password 
@@ -39,7 +41,7 @@ $(function() {
                 message.style.color = goodColor;
                 message.innerHTML = "Passwords Match!"
                 $('#submitReg').prop("disabled", false);
-            }else{
+            } else {
                 //The passwords do not match.
                 //Set the color to the bad color and
                 //notify the user.
@@ -49,7 +51,7 @@ $(function() {
                 $('#submitReg').prop("disabled", true);
             }
     });
-//___________table data populate begin___________________________
+    //___________table data populate begin___________________________
     
 
     $('.go').click(function(){
@@ -67,11 +69,87 @@ $(function() {
             dataType: 'json',
             type: 'POST',
             data: userParam,
-            success: function(data){
+            success: function(data) {
+                //-------put data into table on screen-------------
                 var template = Handlebars.compile( $('#searchResults').html() );
                 for (i in data.results) {
                     $('tbody').append(template(data.results[i]));
-                }
+                    
+                    
+                    //-----------choosing which pin to use for each place---------------
+                    mapPin = "greenmed.png";
+                    // if(tweet_count <= 50) {
+                    //     if(rating <= 1.5) {
+                    //         var mapPin = "redsmall.png";
+                    //     } else if (rating <= 2.5) {
+                    //         var mapPin = "orangesmall.png";
+                    //     } else if (rating <= 3.5) {
+                    //         var mapPin = "yellowsmall.png";
+                    //     } else { 
+                    //         var mapPin = "greensmall.png";
+                    //     }
+
+                    // } else if (tweet_count <= 100) {
+                    //     if(rating <= 1.5) {
+                    //         var mapPin = "redsmed.png";
+                    //     } else if (rating <= 2.5) {
+                    //         var mapPin = "orangemed.png";
+                    //     } else if (rating <= 3.5) {
+                    //         var mapPin = "yellowmed.png";
+                    //     } else { 
+                    //         var mapPin = "greenmed.png";
+                    //     }
+
+                    // } else if (tweet_count <= 150) {
+                    //     if(rating <= 1.5) {
+                    //         var mapPin = "redlarge.png";
+                    //     } else if (rating <= 2.5) {
+                    //         var mapPin = "orangelarge.png";
+                    //     } else if (rating <= 3.5) {
+                    //         var mapPin = "yellowlarge.png";
+                    //     } else { 
+                    //         var mapPin = "greenlarge.png";
+                    //     }
+
+                    // } else {
+                    //     if(rating <= 1.5) {
+                    //         var mapPin = "redxl.png";
+                    //     } else if (rating <= 2.5) {
+                    //         var mapPin = "orangexl.png";
+                    //     } else if (rating <= 3.5) {
+                    //         var mapPin = "yellowxl.png";
+                    //     } else { 
+                    //         var mapPin = "greenxl.png";
+                    //     }
+                    //   }
+
+                    //--------geocode db address----------
+                    var geocoder;
+                    var map;
+                        geocoder = new google.maps.Geocoder();
+                        var latlng = new google.maps.LatLng(33.433702, -111.941948);
+                        var mapOptions = {
+                          zoom: 13,
+                          center: latlng
+                        };
+                        map = new google.maps.Map(document.getElementById('map_div'), mapOptions);
+
+                    address = data.results[i]['street_address'];
+                    geocoder.geocode( { 'address': address}, function(results, status) {
+                      if (status == google.maps.GeocoderStatus.OK) {
+                        map.setCenter(results[0].geometry.location);
+                        //-------defining pin/marker properties-------------
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location,
+                            icon: mapPin,
+                            title:data.results['name']
+                        });
+                      } else {
+                        alert('Geocode was not successful for the following reason: ' + status);
+                      }
+                    });
+                };
             }
 
         });
@@ -79,7 +157,7 @@ $(function() {
 
     });
 
-});
+})(jQuery);
 
 
 
--- map.js
+++ map.js
@@ -1,33 +1,107 @@
-$(function() {
+;var App = App || {};
+(function($) {
 
-var geocoder;
-var map;
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(33.433702, -111.941948);
-  var mapOptions = {
-    zoom: 13,
-    center: latlng
+  //--------geocoder turns location search value into lat/long for maps api-----------
+  var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(33.433702, -111.941948);
+    var mapOptions = {
+      zoom: 13,
+      center: latlng
+    };
+    map = new google.maps.Map(document.getElementById('map_div'), mapOptions);
   };
-  map = new google.maps.Map(document.getElementById('map_div'), mapOptions);
-};
 
-$('.go').click(function() {
-  var address = document.getElementById('zip').value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      // var marker = new google.maps.Marker({
-      //     map: map,
-      //     position: results[0].geometry.location
-      // });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
+  //-----run geocoder when click search button------------------
+  $('.go').click(function() {
+    var address = document.getElementById('zip').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        // var marker = new google.maps.Marker({
+        //     map: map,
+        //     position: results[0].geometry.location,
+        //     icon: mapPin,
+        //     title:data.results['name']
+        // });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
   });
-});
-// $('#zip').on('click', function(){codeAddress();});
-google.maps.event.addDomListener(window, 'load', initialize);
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  // //--------get addresses of each place in search results from database----------
+  // function getAddress (callback){
+  //   $.getJson('http://yoganow-api.herokuapp.com/api/yoga_studios.json?lat=');
+  // };
+
+  // getAddress(function (data){
+  //   var addresses= data.results['address'];
+  //   var address, latLng;
+  //   for(i in address) {
+  //     address = addresses[i];
+  //     latLng = new google.maps.LatLng(address.latitude, address.longtiude)
+
+  //     //-----------choosing which pin to use for each place---------------
+  //     var mapPin = "";
+  //     if(tweet_count <= 50) {
+  //       if(rating <= 1.5) {
+  //         var mapPin = "redsmall.png";
+  //        } else if (rating <= 2.5) {
+  //         var mapPin = "orangesmall.png";
+  //       } else if (rating <= 3.5) {
+  //         var mapPin = "yellowsmall.png";
+  //       } else { 
+  //         var mapPin = "greensmall.png";
+  //       }
+
+  //     } else if (tweet_count <= 100) {
+  //       if(rating <= 1.5) {
+  //         var mapPin = "redsmed.png";
+  //        } else if (rating <= 2.5) {
+  //         var mapPin = "orangemed.png";
+  //       } else if (rating <= 3.5) {
+  //         var mapPin = "yellowmed.png";
+  //       } else { 
+  //         var mapPin = "greenmed.png";
+  //       }
+
+  //     } else if (tweet_count <= 150) {
+  //       if(rating <= 1.5) {
+  //         var mapPin = "redlarge.png";
+  //        } else if (rating <= 2.5) {
+  //         var mapPin = "orangelarge.png";
+  //       } else if (rating <= 3.5) {
+  //         var mapPin = "yellowlarge.png";
+  //       } else { 
+  //         var mapPin = "greenlarge.png";
+  //       }
+
+  //     } else {
+  //       if(rating <= 1.5) {
+  //           var mapPin = "redxl.png";
+  //         } else if (rating <= 2.5) {
+  //           var mapPin = "orangexl.png";
+  //         } else if (rating <= 3.5) {
+  //           var mapPin = "yellowxl.png";
+  //         } else { 
+  //           var mapPin = "greenxl.png";
+  //         }
+  //     }
+
+  //     //-------defining pin/marker properties-------------
+  //     var marker = new google.maps.Marker({
+  //       position:latLng,
+  //       map:map,
+  //       title:studio.name,
+  //       icon:mapPin
+  //     });
+  //   }
+  // });
 
   // // Define Marker properties
   // var image1 = new google.maps.MarkerImage('letter_a.png',
@@ -38,6 +112,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
   //     // The anchor for this image is the base of the flagpole at 18,42.
   //     new google.maps.Point(18, 42)
   // );
+
 
   // // Define Marker properties
   // var image2 = new google.maps.MarkerImage('letter_b.png',
@@ -62,16 +137,14 @@ google.maps.event.addDomListener(window, 'load', initialize);
   //     icon: image2 // This path is the custom pin to be shown. Remove this line and the proceeding comma to use default pin
   // });
 
-// };
+  // };
 
 
 
 
 
-});
 
-
-
+})(jQuery);
 
 
 
