4e3c9d66f4ddf26648a6f7ade9272cd39244385e
Kristine Faultner <kfaultner@kristines-mbp.apollosrc.root>
2014-06-17 15:23:02 -0700

    login email validation done

--- main.js
+++ main.js
@@ -12,14 +12,39 @@
         $(".logToggle").toggle("blind");
     });
 
-    // $(".logOut").click(function(){
-    //     $('.logOutToggle').toggle('blind');
-
-    // });
-
     $( ".toggle" ).hide();
     $(".logToggle").hide();
-    // $(".logOutToggle").hide();
+
+// //--------------Login email validation-------
+    
+    $("#logInEmail").keyup(function(){
+        var emailInput = $('#logInEmail').val();
+        var filter = /^[a-zA-Z-_.+]+@[a-zA-Z-_.+]+\.[a-z]{2,6}\.?[a-z]+/;
+
+        if(!filter.test(emailInput)){
+
+            $('#logInEmail').css('background-color', '#F6CED8');
+       
+        }else {
+            $('#logInEmail').css('background-color', '#64CE7F');
+        }
+
+    });
+
+// //--------------Register email validation-------
+    $("#email").keyup(function(){
+        var emailInput = $('#email').val();
+        var filter = /^[a-zA-Z-_.+]+@[a-zA-Z-_.+]+\.[a-z]{2,6}\.?[a-z]+/;
+
+        if(!filter.test(emailInput)){
+
+            $('#email').css('background-color', '#F6CED8');
+       
+        }else {
+            $('#email').css('background-color', '#64CE7F');
+        }
+
+    });
 
     // //-------password match begin------------
 
