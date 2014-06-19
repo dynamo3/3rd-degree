fd3de0d8d401bea06d5dc2df0437fe4f84b25381
Kristine Faultner <kfaultner@kristines-mbp.apollosrc.root>
2014-06-19 14:24:13 -0700

    client side email reg update

--- main.js
+++ main.js
@@ -67,7 +67,7 @@
 // //--------------Register email validation-------
     $("#email").keyup(function(){
         var emailInput = $('#email').val();
-        var filter = /^[a-zA-Z-_.+]+@[a-zA-Z-_.+]+\.[a-z]{2,6}\.?[a-z]+/;
+        var filter = /^[a-zA-Z0-9-_.+]+@[a-zA-Z-_.+]+\.[a-z]{1,6}\.?[a-z]+$/;
 
         if(!filter.test(emailInput)){
 
